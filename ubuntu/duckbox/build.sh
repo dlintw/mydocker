42ac12b82d62f60f2e9d142562c0cd001b46a1d5
Daniel YC Lin <dlin.tw@gmail>
2014-02-20 07:10:25 +0800

    keep make.log with different timestamp

--- ubuntu/duckbox/build.sh
+++ ubuntu/duckbox/build.sh
@@ -3,5 +3,5 @@ pgrep sshdx>/dev/null && /usr/sbin/sshd -D &
 cd /pdk7105-tdt/tdt/cvs/cdk
 git remote -v
 git pull
-echo "***** `date`" > make.log
-make -j$((`nproc` + 1)) yaud-xbmc-nightly |& tee -a make.log
+make -j$((`nproc` + 1)) yaud-xbmc-nightly \
+  |& tee -a make.`date +%y%m%d%H%M%S`.log
