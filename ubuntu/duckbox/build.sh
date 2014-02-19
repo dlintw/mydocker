24f9a1d762c157313baa053ca52128f8e943e2c7
Daniel YC Lin <dlin.tw@gmail>
2014-02-19 13:38:42 +0800

    enhance build.sh to keep log

--- ubuntu/duckbox/build.sh
+++ ubuntu/duckbox/build.sh
@@ -1,6 +1,7 @@
-/usr/sbin/sshd -D
+# enable ssh login
+pgrep sshdx>/dev/null && /usr/sbin/sshd -D &
 cd /pdk7105-tdt/tdt/cvs/cdk
 git remote -v
 git pull
-make -j$((`nproc` + 1)) yaud-xbmc-nightly
-EOF
+echo "***** `date`" > make.log
+make -j$((`nproc` + 1)) yaud-xbmc-nightly |& tee -a make.log
--- ubuntu/duckbox/run.sh
+++ ubuntu/duckbox/run.sh
@@ -1,3 +1,4 @@
 [ `id -u` != 0 ] && echo "Err: should run as root" &&  exit 1
 [ -d /Archive ] || mkdir /Archive
+echo "Note:After enter docker containner, run ./build.sh to build"
 docker run -t -i -v /Archive:/Archive -p 20022:22 dlin/ubuntu-duckbox /bin/bash
