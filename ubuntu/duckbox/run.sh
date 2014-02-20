71285421195a1ffcabdc1abb844c208c97233d33
Daniel YC Lin <dlin.tw@gmail>
2014-02-20 14:14:31 +0800

    let run.sh accept two optional pararmeters [img] [ssh_port]

--- ubuntu/duckbox/run.sh
+++ ubuntu/duckbox/run.sh
@@ -1,8 +1,10 @@
+# Usage: run.bat [image] [ssh_port]
 [ `id -u` != 0 ] && echo "Err: should run as root" &&  exit 1
 [ -d /Archive ] || mkdir /Archive
 echo "Note:After enter docker containner, run '. /build.sh' to build"
 img=${1:-dlin/ubuntu-duckbox}
-docker run -t -i -v /Archive:/Archive -v /tdt:/pdk7105-tdt -p 20022:22 $img /bin/bash
-#docker run -d -v /Archive:/Archive -v /tdt:/pdk7105-tdt -p 20022:22 $img
+port=${2:-20022}
+docker run -t -i -v /Archive:/Archive -v /tdt:/pdk7105-tdt -p $port:22 $img /bin/bash
+#docker run -d -v /Archive:/Archive -v /tdt:/pdk7105-tdt -p $port:22 $img
 echo "default password is root, change it after login"
-echo "ssh -p20022 root@localhost"
+echo "ssh -p$port root@localhost"
