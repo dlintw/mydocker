cbb0bba4659f99c70e64d1868b7e855e9f4f649d
Daniel YC Lin <dlin.tw@gmail>
2014-02-20 12:38:27 +0800

    skip entry point 22

--- ubuntu/duckbox/run.sh
+++ ubuntu/duckbox/run.sh
@@ -1,7 +1,8 @@
 [ `id -u` != 0 ] && echo "Err: should run as root" &&  exit 1
 [ -d /Archive ] || mkdir /Archive
 echo "Note:After enter docker containner, run '. /build.sh' to build"
-#docker run -t -i -v /Archive:/Archive -v /tdt:/pdk7105-tdt -p 20022:22 dlin/ubuntu-duckbox /bin/bash
-docker run -d -v /Archive:/Archive -v /tdt:/pdk7105-tdt -p 20022:22 ${1:-dlin/ubuntu-duckbox}
+img=${1:-dlin/ubuntu-duckbox}
+docker run -t -i -v /Archive:/Archive -v /tdt:/pdk7105-tdt -p 20022:22 $img /bin/bash
+#docker run -d -v /Archive:/Archive -v /tdt:/pdk7105-tdt -p 20022:22 $img
 echo "default password is root, change it after login"
 echo "ssh -p20022 root@localhost"
