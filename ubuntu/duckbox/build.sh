661b392434fe9260c78d193d4b36a74daa8aac49
Daniel YC Lin <dlin.tw@gmail>
2014-02-20 12:09:31 +0800

    move tdt build directory to outside to prevent 9.8G disk space limitation

--- ubuntu/duckbox/build.sh
+++ ubuntu/duckbox/build.sh
@@ -1,6 +1,8 @@
 # enable ssh login
 pgrep sshd>/dev/null || /usr/sbin/sshd -D &
+[ -d /pdk7105-tdt/.git ] || git clone https://github.com/dlintw/pdk7105-tdt
 cd /pdk7105-tdt/tdt/cvs/cdk
+[ -r Makefile ] || ./make.sh 20 4 y 2 1 2 1 2
 git remote -v
 git pull
 make -j$((`nproc` + 1)) yaud-xbmc-nightly \
--- ubuntu/duckbox/run.sh
+++ ubuntu/duckbox/run.sh
@@ -1,4 +1,7 @@
 [ `id -u` != 0 ] && echo "Err: should run as root" &&  exit 1
 [ -d /Archive ] || mkdir /Archive
 echo "Note:After enter docker containner, run '. /build.sh' to build"
-docker run -t -i -v /Archive:/Archive -p 20022:22 dlin/ubuntu-duckbox /bin/bash
+#docker run -t -i -v /Archive:/Archive -v /tdt:/pdk7105-tdt -p 20022:22 dlin/ubuntu-duckbox /bin/bash
+docker run -d -v /Archive:/Archive -v /tdt:/pdk7105-tdt -p 20022:22 ${1:-dlin/ubuntu-duckbox}
+echo "default password is root, change it after login"
+echo "ssh -p20022 root@localhost"
