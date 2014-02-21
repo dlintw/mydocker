64785f4e7108210c7c48568fdb4a45bb5eeb64c5
Daniel YC Lin <dlin.tw@gmail>
2014-02-21 18:29:25 +0800

    use stocked image

--- ubuntu/duckbox/run.sh
+++ ubuntu/duckbox/run.sh
@@ -5,6 +5,7 @@ echo "Note:After enter docker containner, run '. /build.sh' to build"
 img=${1:-dlin/ubuntu-duckbox}
 port=${2:-20022}
 #docker run -t -i -v /Archive:/Archive -v /tdt:/pdk7105-tdt -p $port:22 $img /bin/bash
-docker run -d -v /Archive:/Archive -v /tdt:/pdk7105-tdt -p $port:22 $img
+docker run -d -v /Archive:/Archive -v /tdt:/pdk7105-tdt -p $port:22 $img \
+  /usr/sbin/sshd -D
 echo "default password is root, change it after login"
 echo "ssh -p$port root@localhost"
