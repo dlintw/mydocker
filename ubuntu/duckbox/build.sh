74e35b8ee45794522fd39057c4342fda79062568
Daniel YC Lin <dlin.tw@gmail>
2014-02-21 10:42:20 +0800

    Append package list

--- ubuntu/duckbox/build.sh
+++ ubuntu/duckbox/build.sh
@@ -4,6 +4,6 @@ pgrep sshd>/dev/null || /usr/sbin/sshd -D &
 cd /pdk7105-tdt/tdt/cvs/cdk
 [ -r Makefile ] || ./make.sh 20 4 y 2 1 2 1 2
 git remote -v
-git pull
+git pull origin master
 make -j$((`nproc` + 1)) yaud-xbmc-nightly \
   |& tee -a make.`date +%y%m%d%H%M%S`.log
