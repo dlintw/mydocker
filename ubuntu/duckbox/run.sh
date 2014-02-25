3c2c6b3f11846fa7d8cf94310960102c4a4ddf0d
Daniel YC Lin <dlin.tw@gmail>
2014-02-25 15:00:36 +0800

    try to build on command line

--- ubuntu/duckbox/run.sh
+++ ubuntu/duckbox/run.sh
@@ -1,13 +1,35 @@
-# Usage: run.bat [image] [ssh_port]
-[ `id -u` != 0 ] && echo "Err: should run as root" &&  exit 1
+if [ $# -eq 0 ] ;then
+  cat <<EOF
+Usage 1: run.sh ssh [image] [ssh_port]
+Usage 2: run.sh build [image] [ssh_port] # automatically build all
+EOF
+  exit 0
+fi
+
+if  ! groups | grep '\bdocker\b' >/dev/null && [ `id -u` != 0 ] ; then
+  echo "Err: should run as root or in group 'docker'" &&  exit 1
+fi
 [ -d /Archive ] || mkdir /Archive
-echo "Note:After enter docker containner, run '. /build.sh' to build"
-img=${1:-dlin/ubuntu-duckbox}
-if [ -n "$2" ] ; then
-  port="-p $2:22"
+[ -d /tdt ] || mkdir /tdt
+
+
+img=${2:-dlin/ubuntu-duckbox}
+if [ -n "$3" ] ; then
+  port="-p $3:22"
 else
   port="-p 22"
 fi
-id=`docker run -d -v /Archive:/Archive -v /tdt:/pdk7105-tdt $port $img /usr/sbin/sshd -D`
-echo "default password is root, change it after login"
-echo "ssh -p`docker port $id 22` root@localhost"
+set -x
+if [ "$1" = build ] ; then
+  docker run -t -i -v /Archive:/Archive -v /tdt:/pdk7105-tdt $port $img bash /build.sh
+elif [ "$1" = ssh ] ; then
+  id=`docker run -d -v /Archive:/Archive -v /tdt:/pdk7105-tdt $port $img /usr/sbin/sshd -D`
+  echo "default password is root, change it after login"
+  echo "ssh -p`docker port $id 22` root@localhost"
+  echo "Note:After enter docker container, run '. /build.sh' to build"
+else
+  echo "Err: invalid option $1"
+  exit 1
+fi
+
+# vim:et sw=2 ts=2 ai:
